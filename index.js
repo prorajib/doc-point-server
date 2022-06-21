c54d3c85f7d6438fdaaa8781439ea373a9af811a
Rajib Chandra Das <prorajib6@gmail.com>
2022-06-21 22:09:23 +0600

    cm

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
  */
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  //credential: admin.credential.cert(serviceAccount),
   credential: admin.credential.applicationDefault(),
 });
 
