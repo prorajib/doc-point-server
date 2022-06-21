645023aa8b96c1901720b27ded77ac2cd5c89efb
Rajib Chandra Das <prorajib6@gmail.com>
2022-06-21 20:28:42 +0600

    server

---
+++ index.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const admin = require('firebase-admin');
+//const { initializeApp } = require('firebase-admin/app');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+//docpoint-firebase-adminsdk.json
+
+//const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+/*
+ */
+
+admin.initializeApp({
+  // credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.applicationDefault(),
+});
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// Database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rpx5h.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const token = req.headers.authorization.split(' ')[1];
+
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(token);
+      req.decodedEmail = decodedUser.email;
+    } catch {}
+  }
+  next();
+}
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('DocPointdb');
+    const appointmentsCollection = database.collection('apointments');
+    const usersCollection = database.collection('users');
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      res.json(result);
+    });
+
+    app.get('/appointments', verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const date = req.query.date;
+
+      const query = { email: email, date: date };
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
+
+    app.get('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      //filter user by email
+      const filter = { email: user.email };
+      // insert as a new user if user does not exits
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    app.put('/users/admin', verifyToken, async (req, res) => {
+      const user = req.body;
+      const requester = req.decodedEmail;
+      if (requester) {
+        const requesterAccount = await usersCollection.findOne({
+          email: requester,
+        });
+        if (requesterAccount.role === 'admin') {
+          const filter = { email: user.email };
+          const updateDoc = {
+            $set: { role: 'admin' },
+          };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+        } else {
+          res
+            .status(403)
+            .json({ message: 'You do not have access to makeAdmin' });
+        }
+      }
+    });
+  } finally {
+    //await client.close();
+  }
+}
+
+run().catch(console.dir);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(` app listening on port ${port}`);
+});
